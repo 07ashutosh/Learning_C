4fa1b0c1624be37f67406726936582f994a6ba07
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-09-08 11:01:11 +0530

    reverse and sum of digits

--- sum_num.c
+++ sum_num.c
@@ -1,16 +1,37 @@
 # include<stdio.h>
+// int main() {
+//     int num;
+//     int sum = 0;
+//     printf("enter the number :");
+//     scanf("%d",&num);
+    
+//     while (num>0){
+//       int endnum =   num %10 ;
+//         sum = (sum + endnum);
+//         num = num/10;
+//     }
+//     printf("sum of digits :%d",sum);
+     
+//     return 0;
+// }
+
+
+//reversing and add the digits
 int main() {
     int num;
+    int rev = 0;
     int sum = 0;
     printf("enter the number :");
     scanf("%d",&num);
     
     while (num>0){
       int endnum =   num %10 ;
+        rev = (rev * 10) + endnum;
         sum = (sum + endnum);
         num = num/10;
     }
-    printf("sum of digits :%d",sum);
+    printf("reversed number is :%d",rev);
+    printf("\nsum of digits :%d",sum);
      
     return 0;
 }
