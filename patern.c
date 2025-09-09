8335a9f47bf3254f420ac425e044c9046f446411
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-09-09 21:27:45 +0530

    number triangle from 1 to 0

--- patern.c
+++ patern.c
@@ -1,10 +1,37 @@
-# include <stdio.h>
+#include <stdio.h>
 
-int main(){
-    for (int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++){
-            printf("*");
+int main()
+{
+    // for (int i=1;i<=5;i++){
+    //     for(int j=1;j<=i;j++){
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+
+    // patern 2
+    // int n = 5;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n - i; j++)
+    //     {
+    //         printf(" ");
+    //     }
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+    int num = 1;
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", num % 10);
+            num++;
         }
         printf("\n");
     }
+    return 0;
 }
