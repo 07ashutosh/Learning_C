3e9af2d4b9c66e823614662d3100677d83e4be2c
Ashutosh Tiwari <ashutoshtiwari07102002@gmail.com>
2025-09-16 10:41:25 +0530

    switch case question

---
+++ SwitchCase.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character (r/g/b/y/o/w): ");
+    scanf(" %c", &ch);
+
+    switch(ch) {
+        case 'r':
+        case 'R':
+            printf("Color: Red\n");
+            break;
+
+        case 'g':
+        case 'G':
+            printf("Color: Green\n");
+            break;
+
+        case 'b':
+        case 'B':
+            printf("Color: Blue\n","Color: Black\n");
+            break;
+
+        case 'y':
+        case 'Y':
+            printf("Color: Yellow\n");
+            break;
+
+        case 'o':
+        case 'O':
+            printf("Color: Orange\n");
+            break;
+
+        case 'w':
+        case 'W':
+            printf("Color: White\n");
+            break;
+
+        default:
+            printf("Invalid input! No color mapped.\n");
+    }
+
+    return 0;
+}
---
+++ primeNo.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main(){
+    int num;
+    printf("For checking prime enter the no:");
+    scanf("%d",&num);
+    for ()
+    if(num%2 != 0){
+        printf(" number is prime :%d",num);
+    }else{
+        printf("number is composite :%d",num);
+    }
+
+}
